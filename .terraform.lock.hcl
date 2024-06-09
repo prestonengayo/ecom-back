d063b3da5f9a67a87ffbf015ad78b087936128f1
Prestone NGAYO <prestonengayo9@gmail.com>
2024-06-09 18:11:10 +0200

    add info into main.tf

---
+++ .terraform.lock.hcl
@@ -0,0 +1,59 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version = "3.107.0"
+  hashes = [
+    "h1:xA73PuMnVaXxtjTkpTJHE+No2IUDgXB4ELw0iKRiDHQ=",
+    "zh:0a5bfcdef1dad509c4f45c0ada2c8e2cc058cf9542ddec48fbee18c4097bce9e",
+    "zh:0b56736691e4b28ea15b381a4711ff39719ff83a40ce97cd283eb21988f471f6",
+    "zh:13d55030c8be466b5de4819e4a8b84da69a40b15bfa0cc2588f5270b4682fa89",
+    "zh:1eac398718cd0973f94015e49ff69a6ed8c860d69e4adbd192c7bea775af2941",
+    "zh:7b1984b60abc7f53298950680bda504eca8d70c9d0d906d6dee2aac6a827f9d6",
+    "zh:86f63ad98576d698c6ba8defa9165160633f086145a1f060014a93f5c2fb384e",
+    "zh:afc78e7e0e76b4d2593ca2ec78b064c896888d03c6cb82f2c5bd37e815e056e7",
+    "zh:b84997b287c673b297ede08404133279dbc72f070c8d6e4284bf62637de4bfb4",
+    "zh:dd1d21c8a37938082a5c2497eacd76bacb1ac459bc9d38ee782443fa87a2247d",
+    "zh:edcaca84c6473427d36f940748e5ce4d1d50b393012f6f6c0ec4303792f607d9",
+    "zh:f0892ecd0eea0c06710056048d8bb75c4c3bda74de7ba41afa60d7b9c9a3b0ca",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.5.1"
+  hashes = [
+    "h1:Np4kERf9SMrqUi7DJ1rK3soMK14k49nfgE7l/ipQ5xw=",
+    "zh:0af29ce2b7b5712319bf6424cb58d13b852bf9a777011a545fac99c7fdcdf561",
+    "zh:126063ea0d79dad1f68fa4e4d556793c0108ce278034f101d1dbbb2463924561",
+    "zh:196bfb49086f22fd4db46033e01655b0e5e036a5582d250412cc690fa7995de5",
+    "zh:37c92ec084d059d37d6cffdb683ccf68e3a5f8d2eb69dd73c8e43ad003ef8d24",
+    "zh:4269f01a98513651ad66763c16b268f4c2da76cc892ccfd54b401fff6cc11667",
+    "zh:51904350b9c728f963eef0c28f1d43e73d010333133eb7f30999a8fb6a0cc3d8",
+    "zh:73a66611359b83d0c3fcba2984610273f7954002febb8a57242bbb86d967b635",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7ae387993a92bcc379063229b3cce8af7eaf082dd9306598fcd42352994d2de0",
+    "zh:9e0f365f807b088646db6e4a8d4b188129d9ebdbcf2568c8ab33bddd1b82c867",
+    "zh:b5263acbd8ae51c9cbffa79743fbcadcb7908057c87eb22fd9048268056efbc4",
+    "zh:dfcd88ac5f13c0d04e24be00b686d069b4879cc4add1b7b1a8ae545783d97520",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.2.2"
+  hashes = [
+    "h1:m467k2tZ9cdFFgHW7LPBK2GLPH43LC6wc3ppxr8yvoE=",
+    "zh:3248aae6a2198f3ec8394218d05bd5e42be59f43a3a7c0b71c66ec0df08b69e7",
+    "zh:32b1aaa1c3013d33c245493f4a65465eab9436b454d250102729321a44c8ab9a",
+    "zh:38eff7e470acb48f66380a73a5c7cdd76cc9b9c9ba9a7249c7991488abe22fe3",
+    "zh:4c2f1faee67af104f5f9e711c4574ff4d298afaa8a420680b0cb55d7bbc65606",
+    "zh:544b33b757c0b954dbb87db83a5ad921edd61f02f1dc86c6186a5ea86465b546",
+    "zh:696cf785090e1e8cf1587499516b0494f47413b43cb99877ad97f5d0de3dc539",
+    "zh:6e301f34757b5d265ae44467d95306d61bef5e41930be1365f5a8dcf80f59452",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:913a929070c819e59e94bb37a2a253c228f83921136ff4a7aa1a178c7cce5422",
+    "zh:aa9015926cd152425dbf86d1abdbc74bfe0e1ba3d26b3db35051d7b9ca9f72ae",
+    "zh:bb04798b016e1e1d49bcc76d62c53b56c88c63d6f2dfe38821afef17c416a0e1",
+    "zh:c23084e1b23577de22603cff752e59128d83cfecc2e6819edadd8cf7a10af11e",
+  ]
+}
--- main.tf
+++ main.tf
@@ -2,29 +2,29 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "ecom" {
+  name     = "ecom-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "ecom" {
+  name                = "ecom-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.ecom.location
+  resource_group_name = azurerm_resource_group.ecom.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "ecom" {
+  name                 = "ecom-subnet"
+  resource_group_name  = azurerm_resource_group.ecom.name
+  virtual_network_name = azurerm_virtual_network.ecom.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "ecom" {
+  name                = "ecom-nsg"
+  location            = azurerm_resource_group.ecom.location
+  resource_group_name = azurerm_resource_group.ecom.name
 
   security_rule {
     name                       = "Allow_SSH"
@@ -51,90 +51,61 @@ resource "azurerm_network_security_group" "example" {
   }
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-publicip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+resource "azurerm_public_ip" "ecom" {
+  name                = "ecom-publicip"
+  location            = azurerm_resource_group.ecom.location
+  resource_group_name = azurerm_resource_group.ecom.name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "ecom" {
+  name                = "ecom-nic"
+  location            = azurerm_resource_group.ecom.location
+  resource_group_name = azurerm_resource_group.ecom.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.ecom.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.ecom.id
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
+resource "azurerm_network_interface_security_group_association" "ecom" {
+  network_interface_id      = azurerm_network_interface.ecom.id
+  network_security_group_id = azurerm_network_security_group.ecom.id
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "ecom" {
+  name                = "ecom-vm"
+  location            = azurerm_resource_group.ecom.location
+  resource_group_name = azurerm_resource_group.ecom.name
+  network_interface_ids = [azurerm_network_interface.ecom.id]
 
-  storage_os_disk {
-    name              = "example_os_disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  size                 = "Standard_DS1_v2"
+  admin_username       = "adminuser"
+  disable_password_authentication = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "adminuser"
-    admin_password = "P@ssw0rd1234!"
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("C:/Users/prest/.ssh/id_rsa.pub")
   }
+}
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-
-    ssh_keys {
-      path     = "/home/adminuser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/id_rsa.pub")
-    }
-  }
-
-  provisioner "file" {
-    source      = "docker-compose.yml"
-    destination = "/home/adminuser/docker-compose.yml"
-
-    connection {
-      type        = "ssh"
-      user        = "adminuser"
-      private_key = file("~/.ssh/id_rsa")
-      host        = azurerm_public_ip.example.ip_address
-    }
-  }
-
-  provisioner "file" {
-    source      = "entrypoint.sh"
-    destination = "/home/adminuser/entrypoint.sh"
-
-    connection {
-      type        = "ssh"
-      user        = "adminuser"
-      private_key = file("~/.ssh/id_rsa")
-      host        = azurerm_public_ip.example.ip_address
-    }
-  }
+resource "null_resource" "provision_vm" {
+  depends_on = [azurerm_linux_virtual_machine.ecom]
 
   provisioner "file" {
     source      = "script.sh"
@@ -143,32 +114,28 @@ resource "azurerm_virtual_machine" "example" {
     connection {
       type        = "ssh"
       user        = "adminuser"
-      private_key = file("~/.ssh/id_rsa")
-      host        = azurerm_public_ip.example.ip_address
+      private_key = file("C:/Users/prest/.ssh/id_rsa")
+      host        = azurerm_public_ip.ecom.ip_address
+      timeout     = "2m"
     }
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /home/adminuser/entrypoint.sh",
       "chmod +x /home/adminuser/script.sh",
-      "sudo /home/adminuser/script.sh"
+      "sudo bash /home/adminuser/script.sh"
     ]
 
     connection {
       type        = "ssh"
       user        = "adminuser"
-      private_key = file("~/.ssh/id_rsa")
-      host        = azurerm_public_ip.example.ip_address
+      private_key = file("C:/Users/prest/.ssh/id_rsa")
+      host        = azurerm_public_ip.ecom.ip_address
+      timeout     = "2m"
     }
   }
-
-  depends_on = [
-    azurerm_network_interface.example,
-    azurerm_public_ip.example
-  ]
 }
 
 output "public_ip" {
-  value = azurerm_public_ip.example.ip_address
+  value = azurerm_public_ip.ecom.ip_address
 }
